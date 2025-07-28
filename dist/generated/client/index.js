5521f9ea55986494f6f78b9bfc35857121d2b8fc
Ben Cuff <bencuff14@gmail.com>
2025-07-28 10:20:30 -0700

    chore: update Prisma Client and Query Engine versions to 6.12.0 and 8047c96 respectively

--- dist/generated/client/index.js
+++ dist/generated/client/index.js
@@ -35,12 +35,12 @@ exports.Prisma = Prisma
 exports.$Enums = {}
 
 /**
- * Prisma Client JS version: 6.11.1
- * Query Engine version: f40f79ec31188888a2e33acda0ecc8fd10a853a9
+ * Prisma Client JS version: 6.12.0
+ * Query Engine version: 8047c96bbd92db98a2abc7c9323ce77c02c89dbc
  */
 Prisma.prismaVersion = {
-  client: "6.11.1",
-  engine: "f40f79ec31188888a2e33acda0ecc8fd10a853a9"
+  client: "6.12.0",
+  engine: "8047c96bbd92db98a2abc7c9323ce77c02c89dbc"
 }
 
 Prisma.PrismaClientKnownRequestError = PrismaClientKnownRequestError;
@@ -135,7 +135,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "/Users/bcuff/codepath/clueless-interations/dist/generated/client",
+      "value": "/Users/bcuff/codepath/clueless/clueless-interaction-client/dist/generated/client",
       "fromEnvVar": null
     },
     "config": {
@@ -153,16 +153,15 @@ const config = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "/Users/bcuff/codepath/clueless-interations/prisma/schema.prisma",
+    "sourceFilePath": "/Users/bcuff/codepath/clueless/clueless-interaction-client/prisma/schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
-    "schemaEnvPath": "../../../.env"
+    "rootEnvPath": null
   },
   "relativePath": "../../../prisma",
-  "clientVersion": "6.11.1",
-  "engineVersion": "f40f79ec31188888a2e33acda0ecc8fd10a853a9",
+  "clientVersion": "6.12.0",
+  "engineVersion": "8047c96bbd92db98a2abc7c9323ce77c02c89dbc",
   "datasourceNames": [
     "db"
   ],
